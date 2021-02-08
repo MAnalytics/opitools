5a8ff342dd4899de6919dc00d0008702d466314f
geoMADE <geomad@leeds.ac.uk>
2021-02-08 15:46:16 +0000

    uploaded

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Sample Twitter data
+#'
+#' A dataframe of containing anonymized tweets download
+#' within a geographical coverage.
+#'
+#' @format A dataframe containing one variable:
+#' \itemize{
+#'   \item text: tweet texts
+#'     }
+"tweets"
