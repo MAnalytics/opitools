b21cc6518e60bb203731314b95818f4546ea7306
geoMADE <geomad@leeds.ac.uk>
2021-03-01 21:31:09 +0000

    adding afinn lexicon

---
+++ R/afinn_111.R
@@ -0,0 +1,11 @@
+#' @title Lexicon AFINN (word list and sentiment scale)
+#' @description A list of lexicon of English words rated for valence with
+#' an integer between minus five (negative) and plus five (positive).
+#' The words have been manually labeled by Nielsen, F. A. (2011).
+#' @name afinn_111
+#' @docType data
+#' @usage afinn_111
+#' @format A list (dataframe)
+#' @keywords datasets
+
+NULL
