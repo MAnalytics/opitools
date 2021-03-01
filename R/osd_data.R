7943b3d196bbfd28a0bdc0d7f37ab3a7dc6ad005
geoMADE <geomad@leeds.ac.uk>
2021-03-01 21:30:35 +0000

    update osd_data.R

--- R/osd_data.R
+++ R/osd_data.R
@@ -2,8 +2,8 @@
 #' @description A tidy-format list (dataframe) showing the resulting
 #' classification of each text records into positive, negative
 #' or neutral sentiment. The second column of the dataframe consists of
-#' labels variables `present` and `absent` to indicate whether any of the secondary
-#' keywords exist in a text record.
+#' labels variables `present` and `absent` to indicate whether any
+#' of the secondary keywords exist in a text record.
 #' @name osd_data
 #' @docType data
 #' @usage osd_data
