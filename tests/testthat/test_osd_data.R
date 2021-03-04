e8704332678616293eb48efa3a1c680fd597cfa8
geoMADE <geomad@leeds.ac.uk>
2021-03-04 12:32:56 +0000

    remove all test files

--- tests/testthat/test_osd_data.R
+++
@@ -1,8 +0,0 @@
-context("Testing osd_data.R function")
-
-test_that('data types, dimension of test data', {
-  expect_is(osd_data,'data.frame')
-  expect_equal(nrow(osd_data), 1465)
-  expect_equal(ncol(osd_data), 3)
-})
-
--- tests/testthat/test_policing_otd.R
+++
@@ -1,7 +0,0 @@
-context("Testing policing_otd.R function")
-
-test_that('data types, dimension of test data', {
-  expect_is(policing_otd,'data.frame')
-  expect_equal(nrow(policing_otd), 110)
-  expect_equal(ncol(policing_otd), 1)
-})
