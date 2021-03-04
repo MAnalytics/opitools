56fdfd85d867a0afb0d57ac24bfd460a39500bde
geoMADE <geomad@leeds.ac.uk>
2021-03-04 13:03:06 +0000

    Removing CRAN formatted data

--- R/covid_keys.R
+++
@@ -1,9 +0,0 @@
-#' @title COVID-19 pandemic related keywords
-#' @description A list of keywords relating to the COVID-19 pandemic
-#' @name covid_keys
-#' @docType data
-#' @usage covid_keys
-#' @format A list (dataframe)
-#' @keywords datasets
-
-NULL
