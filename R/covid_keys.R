f919d3e81e2bddec2217c19e8cfacdf93e0e55b4
geoMADE <geomad@leeds.ac.uk>
2021-03-05 21:18:12 +0000

    adding data files

---
+++ R/covid_keys.R
@@ -0,0 +1,9 @@
+#' @title COVID-19 pandemic related keywords
+#' @description A list of keywords relating to the COVID-19 pandemic
+#' @name covid_keys
+#' @docType data
+#' @usage covid_keys
+#' @format A list (dataframe)
+#' @keywords datasets
+
+NULL
---
+++ R/tweets.R
@@ -0,0 +1,15 @@
+#' @title Fake Twitter posts on police/policing 2
+#' @description A sample text document (an OTD) containing
+#' twitter posts (from an anonymous geographical location 2).
+#' The primary subject of the document is the “process of
+#' policing” (as the primary subject). The OTD is believed
+#' to also include posts that express sentiments on the
+#' COVID-19 pandemic (Secondary subject B) in relation
+#' to the primary subject.
+#' @name tweets
+#' @docType data
+#' @usage tweets
+#' @format A list(dataframe)
+#' @keywords datasets
+
+NULL
