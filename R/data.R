4d5e88bdec4ff36e7e23ff1fe1112a69857e74b7
geoMADE <geomad@leeds.ac.uk>
2021-07-05 01:28:55 +0100

    update data

--- R/data.R
+++ R/data.R
@@ -31,8 +31,8 @@
 #' Fake Twitter posts on police/policing
 #'
 #' A text document (an OTD) containing twitter posts
-#' (for an anonymous geographical location 1) on police/policing
-#' (primary subject A). The OTD also includes
+#' (for an anonymous geographical location 1) on police/policing.
+#' The OTD also includes
 #' posts that express sentiments on policing in relation to
 #' the COVID-19 pandemic (Secondary subject B)
 #'
